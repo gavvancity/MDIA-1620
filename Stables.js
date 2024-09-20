f05472a8d35648f2e2d4da4cd399b193cd0560b6
Gavin Doyle <gavindoyle@Gavins-MacBook-Pro-2.local>
2024-09-20 15:38:25 -0700

    initialize stables

---
+++ Stables.js
@@ -0,0 +1,13 @@
+let horseName = "Cool Horsey Fella"
+
+console.log(horseName);
+horseName = "Quentin Tarantino"
+console.log(horseName);
+
+let horseAge = 10;
+
+console.log(horseAge)
+
+let isHorseInside = true;
+
+console.log
